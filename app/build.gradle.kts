5dd5daf32ead6287d263ce1c9f00796acdfe7bfa
Mateusz Dziubek <>
2024-08-07 16:04:05 +0200

    Update versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
 
     defaultConfig {
         applicationId = "com.moodline"
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
